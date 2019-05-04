3e1ef6bdbbfdd84028ff1d61b83ed06491aecd83
AbimanyuB <abimanyu.bhamakerti@gmail.com>
2019-05-04 15:48:24 +0700

    Second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './components/Person/Person'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, i'm a React App</h1>
+      <p>This is really working!</p>
+      <Person />
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+    return <p>Hi, I'm a Person</p>
+}
+
+export default Person;
